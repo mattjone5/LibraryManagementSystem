1b1dd83b86b8bafaca025b8a7967b4ad5953ccaf
Matt J <mattdog13pokemon@gmail.com>
2024-01-23 13:20:02 -0700

    Added a little bit of code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
 # I am switching this to python as I feel like it will make more sense in the scope of it all
+import mysql.connector
+cnx = mysql.connector.connect(user="root", password='root', host='127.0.0.1', database="librarysystem")
+c = cnx.cursor()
+print(c.execute("SELECT * FROM users;"))
+cnx.close()
