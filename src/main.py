a6cb3c6a99908bf013a34492a38d57233cb926dd
Matt J <mattdog13pokemon@gmail.com>
2024-01-26 13:12:03 -0700

    Added code for logging into the system. So far, that code works

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
 # I am switching this to python as I feel like it will make more sense in the scope of it all
- #threading.Thread(target= lambda: winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)).start() 
-# Above is me just testing some code since I haven't touched the above in a while
 
-# Remove above code when needed
 from tkinter import *
+from tkinter import messagebox
 import winsound
 import threading 
 import sys
 import tools
+import hashlib
 
 class Menu(Frame):
+
+    db = None
+    username = None
 
     def __init__(self, parent=None):
         Frame.__init__(self,parent)
         self.pack()
-        self.username = ""
-        self.db = tools.DBConnection()
+        Menu.db = tools.DBConnection()
 
     def makeMenu(self): # This is going to edit the root itself as we want that to always have the menu. I might change this later. The idea right now is that buttons will open toplevel Tk's
         sysLabel = Label(self, text="Library Management System")
@@ -24,7 +25,7 @@ class Menu(Frame):
         checkoutButton = Button(self, text="Check Out") #TODO add command
         checkbookscuroutButton = Button(self, text="Check what books you have out") #TODO add command
         checkaccountButton = Button(self, text="Check Account") #TODO add command
-        quitappButton = Button(self, text="Quit App", command=lambda:sys.exit(0)) #TODO add command
+        quitappButton = Button(self, text="Quit App", command=lambda: (Menu.db.cnx.close(), sys.exit(0))) #TODO add command
         sysLabel.pack()
         loginButton.pack()
         checkinButton.pack()
@@ -33,28 +34,40 @@ class Menu(Frame):
         checkaccountButton.pack()
         quitappButton.pack()
 
+    def checkSignin(username, password, window):
+        passwordHash = hashlib.sha256(password.get().encode('utf-8')).hexdigest()
+        data = Menu.db.getLogInData(username, passwordHash)
+        if data != None:
+            Menu.username = data[0][1]
+            window.destroy()
+        else:
+            threading.Thread(target= lambda: winsound.PlaySound("SystemHand", winsound.SND_ALIAS)).start()
+            messagebox.showerror("Unable to Log in","Error:\nUsername or Passwor is incorrect. Please try again!")
+
     def login():
         top = Toplevel()
         top.title("Account Set up/Sign in")
+        username = StringVar()
+        password = StringVar()
         usernameLabel = Label(top,text="Username: ")
-        usernameEntry = Entry(top)
+        usernameEntry = Entry(top, textvariable=username)
         passwordLabel = Label(top, text="Password")
-        passwordEntry = Entry(top)
-        signinButton = Button(top, text="Sign in")
+        passwordEntry = Entry(top, textvariable=password, show="*")
+        signinButton = Button(top, text="Sign in", command=lambda: Menu.checkSignin(username, password, top))
         signupButton = Button(top, text="Sign up")
         usernameLabel.pack()
         usernameEntry.pack()
         passwordLabel.pack()
         passwordEntry.pack()
+        signinButton.pack()
+        signupButton.pack()
 
-class EventHandler():
-    pass
-        
-
+    def signup():
+        pass
 
 if __name__ == "__main__":
     root = Tk()
     app = Menu(root)
-    app.master.title("Testing")
+    app.master.title("Library System")
     app.makeMenu()
-    app.mainloop() 
+    app.mainloop()
--- src/tools.py
+++ src/tools.py
@@ -1,12 +1,22 @@
 import mysql.connector
+import sys
 class DBConnection():
     def __init__(self):
+        cnx = None
         try:
-            self.cnx = mysql.connector.connect(user="remoteUser", password='root', host='192.168.1.193',  database="librarySystem")
+            DBConnection.cnx = mysql.connector.connect(user="remoteUser", password='root', host='192.168.1.193',  database="librarySystem")
             print ("Connection successful!")
         except:
             print("Error, connection failed!")
+            sys.exit(0)
+            
+    def getLogInData(self, username, password):
+        data = []
+        cursor = DBConnection.cnx.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s;", (username.get(), password))
+        for dataPoint in cursor:
+            data.append(dataPoint)
+        if len(data) == 0:
+            return None
+        return data
 
-    def testStuff(self):
-        cursor = self.cnx.cursor()
-        print(cursor.execute("SELECT * FROM users;"))
